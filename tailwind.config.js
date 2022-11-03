8cea5746fdb9cefb364cec40ee503a1c372f9d9f
Desmond <desmondfon@yahoo.com>
2022-11-03 15:43:45 +0100

    completed the mobile view

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        'nunito': ['Nunito Sans', 'sans-serif'],
+      },
+      colors: {
+        'darkBlue': 'hsl(209, 23%, 22%)',
+        'veryDarkBlueDM': 'hsl(207, 26%, 17%)',
+        'veryDarkBlueLM': 'hsl(200, 15%, 8%)',
+        'darkGrayLM': 'hsl(0, 0%, 52%)',
+        'veryLightGrayLM': 'hsl(0, 0%, 98%)',
+        'whiteLMDM': 'hsl(0, 0%, 100%)'
+      },
+    },
+  },
+  plugins: [],
+}
